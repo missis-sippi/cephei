28e0e9986c10b084f730e092a2a841e4a87216f6
Ilya Shabanov <ishaba@me.com>
2015-08-12 17:50:28 +0300

    Remove old ie support

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,6 @@
-Modernizr.load([
-  {
-    test: Modernizr.placeholder,
-    nope: 'scripts/placeholders.min.js'
-  },{
-    test: Modernizr.touch,
-    yep: 'scripts/fastclick.min.js',
-    complete: function(){
-      if(Modernizr.touch)
-        FastClick.attach(document.body);
-    }
-  }
-]);
-
 $(function() {
+
+  if(Modernizr.touch)
+    FastClick.attach(document.body);
 
 });
